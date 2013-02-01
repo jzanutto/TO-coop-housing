1cd21f5079d33d3e09357ec6c5cf7c4f74c5c389
jzanutto <jzanutto@live.com>
2013-02-01 17:56:38 -0500

    init

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+TOCoopHousing::Application.config.secret_token = '9dfbc0a6113fd217b7cbe9ebd138df006ca93a826f0e3baccbc837b0ac07c6736d6e4a09589caea4c74bb52b58a20a5ce18f88888d9570a5a463a28647123a48'
