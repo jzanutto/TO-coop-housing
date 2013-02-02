2b100ca90e3df4ccffec608d451659a9619ddfb8
jzanutto <jzanutto@live.com>
2013-02-01 22:27:27 -0500

    added validators to the model

--- app/models/company.rb
+++ app/models/company.rb
@@ -1,3 +1,6 @@
 class Company < ActiveRecord::Base
   attr_accessible :Lat, :Location, :Long, :Name, :Postal
+  validates :Name, :presence=>true
+  validates :Lat, :presence=>true
+  validates :Long, :presence=>true
 end
--- app/models/housing.rb
+++ app/models/housing.rb
@@ -1,3 +1,5 @@
 class Housing < ActiveRecord::Base
   attr_accessible :Lat, :Location, :Long, :Postal, :Price
+  validates :Lat, :presence=>true
+  validates :Long, :presence=>true
 end
